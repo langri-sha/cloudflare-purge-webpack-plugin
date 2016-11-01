34859b2ed162dea9cf19f288dbca9e51dbfb66ad
Filip Dupanović <filip.dupanovic@gmail.com>
2016-11-01 15:44:13 +0100

    feat(src): Setup base

---
+++ test.js
@@ -0,0 +1,61 @@
+const MemoryFs = require('memory-fs')
+const test = require('ava')
+const webpack = require('webpack')
+
+function compile (plugin = null) {
+  const compiler = webpack({
+    entry: [
+      'foo.txt',
+      'bar.txt'
+    ],
+    resolve: {
+      modules: ['./fixtures']
+    },
+    output: {
+      path: '/',
+      filename: 'index.js',
+      publicPath: 'http://example.com'
+    },
+    module: {
+      rules: [{
+        test: /\.txt$/,
+        use: 'raw'
+      }]
+    },
+    plugins: plugin && [plugin] || []
+  })
+  compiler.fileSystem = new MemoryFs();
+
+  return new Promise((resolve, reject) => {
+    compiler.run((err, stats) => {
+      if (err) return reject(err)
+
+      if (stats.hasErrors() || stats.hasWarnings()) {
+        return reject(new Error(stats.toString({
+          errorDetails: true,
+          warnings: true
+        })))
+      }
+
+      resolve({compiler, stats})
+    })
+  })
+}
+
+test('Test Webpack compiler setup', async t => {
+  let compiler, stats
+  t.plan(2)
+
+  t.notThrows(async () => {
+    await compile()
+  })
+
+  await compile(new class {
+    apply(compiler) {
+      compiler.plugin('emit', (compilation, callback) => {
+        t.pass()
+        callback()
+      })
+    }
+  })
+})
